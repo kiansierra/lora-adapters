f31cf0bd4b79377a9abb910b801d9c9a97c344a0
kiansierra <kiansierra90@gmail.com>
2023-05-02 02:03:15 +0200

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="lora_adapters",
-    version="0.1.1",
+    version="0.1.2",
     author="Kian Sierra McGettigan",
     author_email="kiansierra90@gmail.com",
     description="PyTorch implementation of low-rank adaptation (LoRA), a parameter-efficient approach to adapt a large pre-trained deep learning model which obtains performance on-par with full fine-tuning.",
@@ -19,5 +19,5 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
 )
